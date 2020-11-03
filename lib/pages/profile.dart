73ca12bf5e7f896ed63b2de27c5eab056eb7dc73
FikryAS <azizfikry15@gmail.com>
2020-11-03 14:51:33 +0700

    add passing data and image

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class Profile extends StatefulWidget {
+  final String name;
+
+  Profile({@required this.name});
+
   @override
   _ProfileState createState() => _ProfileState();
 }
@@ -13,18 +17,30 @@ class _ProfileState extends State<Profile> {
       appBar: AppBar(
         leading: BackButton(
           onPressed: () {
+            Navigator.pop(context); // berpindah page ke page sebelumnya
             print('Back top previous page');
           },
         ),
         title: Text('Profile'),
+        centerTitle: true,
       ),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            CircleAvatar(
+              radius: 75,
+              // digunakan untuk mengambil gambar di internet
+              backgroundImage: NetworkImage(
+                  'https://animenewsandfacts.com/wp-content/uploads/2020/10/Adachi-to-Shimamura-anime-.jpg'),
+            ),
+            SizedBox(
+              height: 25,
+            ),
             Center(
                 child: Text(
-              'Your Name',
-              style: TextStyle(fontSize: 20),
+              widget.name, //mengambil name dari variabel dari class profile
+              style: TextStyle(fontSize: 25),
             ))
           ],
         ),
