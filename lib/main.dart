da052f50b6d7fb0f1de16895fe52a2008c7a4c08
FikryAS <azizfikry15@gmail.com>
2020-11-03 11:54:27 +0700

    add navigations and notifications widget

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:aplikasi_navigation_mobprog2/pages/edit_profil.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: EditProfile(),
+    );
+  }
+}
---
+++ lib/pages/edit_profil.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class EditProfile extends StatefulWidget {
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  TextEditingController nameController = new TextEditingController();
+  final globalKey = GlobalKey<ScaffoldState>(); // pendelkarasian context global
+
+  // peggunaan notifikasi toastbar
+  showToast(text) {
+    Fluttertoast.showToast(
+        msg: text,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black.withOpacity(0.5),
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  // penggunaan notifikasi snackbar
+  showSnackBar(text) {
+    final snackbar = SnackBar(
+      content: Text(text),
+      action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            print('Clicked!');
+          }),
+    );
+    globalKey.currentState.showSnackBar(snackbar);
+  }
+
+  // penggunaan notifikasi alert dialog
+  showAlertDialogMaterial(text) {
+    showDialog(
+      context: globalKey.currentContext,
+      builder: (_) => AlertDialog(
+        title: Text('Info'),
+        content: Text(text),
+        actions: [
+          FlatButton(
+            onPressed: () {
+              Navigator.pop(globalKey.currentContext); // pop = kembali ke menu awal
+              print('Clicked No');
+            }, 
+            child: Text('No')),
+          FlatButton(
+            onPressed: () {
+              print('Clicked Yes')   ;
+            }, 
+            child: Text('Yes'))
+        ],
+        elevation: 5, // menampilkan bayangan di setiap sisi  widget
+        backgroundColor: Colors.grey,
+      ),
+      barrierColor: Colors.black.withOpacity(0.5),
+      barrierDismissible: false // pada saat klik sembarang alert dialog tidak akan tertutup
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: globalKey, // memanggil context
+      body: Container(
+          padding: EdgeInsets.only(top: 64, bottom: 64, left: 24, right: 24),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment
+                  .spaceBetween, // membagi rata space yang tersedia untuk widget
+              crossAxisAlignment: CrossAxisAlignment
+                  .stretch, //memenuhi layar kanan kiri suatu widget
+              children: [
+                Container(
+                  child: Text(
+                    'EDIT PROFIL',
+                    style: TextStyle(fontSize: 40, color: Colors.blueAccent),
+                  ),
+                ),
+                Container(
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 48,
+                  child: FlatButton(
+                    color: Colors.blueAccent,
+                    onPressed: () {
+                      if (nameController.text == '') {
+                        // showToast('Please input your name!');
+                        // showSnackBar('Please input your name!');
+                        showAlertDialogMaterial('Please input your name!');
+                      }
+                    },
+                    child: Text('Update',
+                        style: TextStyle(color: Colors.white, fontSize: 14)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24)),
+                  ),
+                )
+              ])),
+    );
+  }
+}
